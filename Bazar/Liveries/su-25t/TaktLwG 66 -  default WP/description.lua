3dd47e75a0944de87da812d0cc75c21e4d592a97
Basti <giveafuck@gmail.com>
2021-02-03 00:14:19 +0100

    SU-25t livery added

---
+++ Bazar/Liveries/su-25t/TaktLwG 66 -  default WP/description.lua
@@ -0,0 +1,16 @@
+livery = {
+	{"fuz-center", 0, "SU39-PAINT1-DEF-02", true},
+	{"wings", 0, "../TaktLwG 66 -  default/wings", false},
+	{"SU39-PAINT2bis-DEF-01", 0, "SU39-PAINT2bis-DEF-02", true},
+	{"fuz-tail & fin", 0, "fuz-tail_fin", false},
+	{"tail & stab", 0, "SU39-PAINT4-DEF-02", true},
+	{"brake-L", 0, "SU39-PAINT5-L-DEF-02", true},
+	{"brake-R", 0, "SU39-PAINT5-R-DEF-02", true},
+	{"fuz-nose-steel", 0, "../TaktLwG 66 -  default/fuz-nose-steel", false},
+	{"SU-39-PILON", 0, "SU39-PILON-PAINT-DEF-02", true},
+	{"PILON_2_Su-39", 0, "SU39-PILON-2-PAINT-DEF-02", true},
+	{"numbers", 0, "../TaktLwG 66 -  default/numbers", false},
+	{"PylonsSu-25T1", 0, "Su-25TPylon2", true},
+	{"pylon25lateral", 0, "Lateral25TPylon2", true},
+}
+name = "TaktLwG 66 -  default WP"

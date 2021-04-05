f9b15c86390e1a373bb00b1e8dcb90a556bfe5e7
Basti <giveafuck@gmail.com>
2021-04-06 00:10:52 +0200

    Small fixes

--- CoreMods/aircraft/ChinaAssetPack/Liveries/JF-17/TaktLwG 66 - special/description.lua
+++ CoreMods/aircraft/ChinaAssetPack/Liveries/JF-17/TaktLwG 66 - special/description.lua
@@ -1,43 +1,43 @@
 livery = {
     {"JF-17_main01", 0, "JF-17_main01", false},
-	{"JF-17_main01", ROUGHNESS_METALLIC ,"JF-17_main01_roughmet.dds",false},
+	{"JF-17_main01", ROUGHNESS_METALLIC ,"JF-17_main01_roughmet.dds",true},
     {"JF-17_main01_BN52", 0, "JF-17_main01", false},
-	{"JF-17_main01_BN52", ROUGHNESS_METALLIC ,"JF-17_main01_roughmet.dds",false},
+	{"JF-17_main01_BN52", ROUGHNESS_METALLIC ,"JF-17_main01_roughmet.dds",true},
     {"JF-17_main01_BN31", 0, "JF-17_main01", false},
-	{"JF-17_main01_BN31", ROUGHNESS_METALLIC ,"JF-17_main01_roughmet.dds",false},
+	{"JF-17_main01_BN31", ROUGHNESS_METALLIC ,"JF-17_main01_roughmet.dds",true},
     {"JF-17_main01_BN32", 0, "JF-17_main01", false},
-	{"JF-17_main01_BN32", ROUGHNESS_METALLIC ,"JF-17_main01_roughmet.dds",false},
+	{"JF-17_main01_BN32", ROUGHNESS_METALLIC ,"JF-17_main01_roughmet.dds",true},
     {"JF-17_main01_BN52", 3, "JF-17_numbers01", false},
     {"JF-17_main01_BN31", 3, "JF-17_numbers01", false},
     {"JF-17_main01_BN32", 3, "JF-17_numbers01", false},
     {"JF-17_main02", 0, "JF-17_main02", false},
-	{"JF-17_main02", ROUGHNESS_METALLIC ,"JF-17_main02_roughmet.dds",false},
+	{"JF-17_main02", ROUGHNESS_METALLIC ,"JF-17_main02_roughmet.dds",true},
     {"JF-17_main03", 0, "JF-17_main03", false},
-	{"JF-17_main03", ROUGHNESS_METALLIC ,"JF-17_main03_roughmet.dds",false},
+	{"JF-17_main03", ROUGHNESS_METALLIC ,"JF-17_main03_roughmet.dds",true},
     {"JF-17_main04", 0, "JF-17_main04", false},
-	{"JF-17_main04", ROUGHNESS_METALLIC ,"JF-17_main04_roughmet.dds",false},
+	{"JF-17_main04", ROUGHNESS_METALLIC ,"JF-17_main04_roughmet.dds",true},
     {"JF-17_main05", 0, "JF-17_main05", false},
-	{"JF-17_main05", ROUGHNESS_METALLIC ,"JF-17_main05_roughmet.dds",false},
+	{"JF-17_main05", ROUGHNESS_METALLIC ,"JF-17_main05_roughmet.dds",true},
     {"JF-17_main06", 0, "JF-17_main06", false},
-	{"JF-17_main06", ROUGHNESS_METALLIC ,"JF-17_main06_roughmet.dds",false},
+	{"JF-17_main06", ROUGHNESS_METALLIC ,"JF-17_main06_roughmet.dds",true},
     {"JF-17_main07", 0, "JF-17_main07", false},
-	{"JF-17_main07", ROUGHNESS_METALLIC ,"JF-17_main07_roughmet.dds",false},
+	{"JF-17_main07", ROUGHNESS_METALLIC ,"JF-17_main07_roughmet.dds",true},
     {"JF-17_main08", 0, "JF-17_main08", false},
-	{"JF-17_main08", ROUGHNESS_METALLIC ,"JF-17_main08_roughmet.dds",false},
+	{"JF-17_main08", ROUGHNESS_METALLIC ,"JF-17_main08_roughmet.dds",true},
 	{"JF-17_main08_decol", 0, "JF-17_main08", false},
-	{"JF-17_main08_decol", ROUGHNESS_METALLIC ,"JF-17_main08_roughmet.dds",false},
+	{"JF-17_main08_decol", ROUGHNESS_METALLIC ,"JF-17_main08_roughmet.dds",true},
 	{"JF-17_main08_decol", 3, "JF-17_decol_DV", false},
 	{"JF-17_main08_BN52", 0, "JF-17_main08", false},
-	{"JF-17_main08_BN52", ROUGHNESS_METALLIC ,".JF-17_main08_roughmet.dds",false},
+	{"JF-17_main08_BN52", ROUGHNESS_METALLIC ,".JF-17_main08_roughmet.dds",true},
 	{"JF-17_main08_BN31", 0, "JF-17_main08", false},
-	{"JF-17_main08_BN31", ROUGHNESS_METALLIC ,"JF-17_main08_roughmet.dds",false},
+	{"JF-17_main08_BN31", ROUGHNESS_METALLIC ,"JF-17_main08_roughmet.dds",true},
 	{"JF-17_main08_BN32", 0, "JF-17_main08", false},
-	{"JF-17_main08_BN32", ROUGHNESS_METALLIC ,"JF-17_main08_roughmet.dds",false},
+	{"JF-17_main08_BN32", ROUGHNESS_METALLIC ,"JF-17_main08_roughmet.dds",true},
 	{"JF-17_main08_BN52", 3, "JF-17_numbers01", false},
 	{"JF-17_main08_BN31", 3, "JF-17_numbers01", false},
 	{"JF-17_main08_BN32", 3, "JF-17_numbers01", false},
     {"JF-17_main09", 0, "JF-17_main09", false},
-	{"JF-17_main09", ROUGHNESS_METALLIC ,"JF-17_main09_roughmet.dds",false},
+	{"JF-17_main09", ROUGHNESS_METALLIC ,"JF-17_main09_roughmet.dds",true},
     {"JF-17_numbers02_BN52", 0, "empty", true},
 	{"JF-17_numbers02_BN31", 0, "empty", true},
 	{"JF-17_numbers02_BN32", 0, "empty", true},
@@ -50,9 +50,9 @@ livery = {
 	{"BRU_33AA", NORMAL_MAP, "bru_33aa_nm", true};
 	{"BRU_33AA", ROUGHNESS_METALLIC, "bru_33aa_diff_roughmet", true};
 	{"Pylon_PF12Twin", 0, "jf-17_dual_pf", false};
-	{"Pylon_PF12Twin", ROUGHNESS_METALLIC, "JF-17_Dual_PF_RoughMet", false};
+	{"Pylon_PF12Twin", ROUGHNESS_METALLIC, "JF-17_Dual_PF_RoughMet", true};
 	{"Pylon_PF-12", 0, "jf-17_pf12dual", false};
-	{"Pylon_PF-12", ROUGHNESS_METALLIC, "JF-17_PF12Dual_RoughMet", false};
+	{"Pylon_PF-12", ROUGHNESS_METALLIC, "JF-17_PF12Dual_RoughMet", true};
 	{"JF-17_PF-9W",	    0			,	"jf-17_pf9w",     false};
 	{"JF-17_PF-9WR",	0			,	"jf-17_pf9w",     false};
 	{"Tank_800", DIFFUSE, "tank800", false};

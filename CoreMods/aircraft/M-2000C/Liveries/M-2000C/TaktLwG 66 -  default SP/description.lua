70e1e66be7b391c3e96ec7b2bed259363fb9406f
Basti <giveafuck@gmail.com>
2021-05-13 01:45:22 +0200

    Adding M-2000 and F-14 livery for MAC

---
+++ CoreMods/aircraft/M-2000C/Liveries/M-2000C/TaktLwG 66 -  default SP/description.lua
@@ -0,0 +1,76 @@
+livery = {
+	{"ext1", 0 ,"../TaktLwG 66 -  default/M2KC_1T_ada_ch25",false};
+	{"ext2", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"ext6", 0 ,"../TaktLwG 66 -  default/M2KC_6T",false};
+--	{"pilot_M2KC_helmet", 0 ,"../TaktLwG 66 -  default/Pilot_M2KC_helmet_NAME-HERE",false};
+--	{"pilot_M2KC", 0 ,"../TaktLwG 66 -  default/Pilot_M2KC_ada_ch25",false};
+--	{"pilot_M2KC_patch", 0 ,"../TaktLwG 66 -  default/Pilot_M2KC_patch_NAME-HERE",false};
+
+	{"FUSE_NUMBER1_001", 0 ,"../TaktLwG 66 -  default/M2KC_1T_ada_ch25",false};
+	{"FUSE_NUMBER1_001", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"FUSE_NUMBER2_001", 0 ,"../TaktLwG 66 -  default/M2KC_1T_ada_ch25",false};
+	{"FUSE_NUMBER2_001", DECAL ,"../TaktLwG 66 -  default/M2KC_N2_ada_ch25",false};
+
+	{"FUSE_NUMBER2_010", 0 ,"../TaktLwG 66 -  default/M2KC_1T_ada_ch25",false};
+	{"FUSE_NUMBER2_010", DECAL ,"../TaktLwG 66 -  default/M2KC_N2_ada_ch25",false};
+
+	{"TAIL_NUMBER1_001", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"TAIL_NUMBER1_001", DECAL ,"../TaktLwG 66 -  default/M2KC_N2_ada_ch25",false};
+
+	{"TAIL_NUMBER1_010", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"TAIL_NUMBER1_010", DECAL ,"../TaktLwG 66 -  default/M2KC_N2_ada_ch25",false};
+
+	{"TAIL_NUMBER1_100", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"TAIL_NUMBER1_100", DECAL ,"../TaktLwG 66 -  default/M2KC_N2_ada_ch25",false};
+
+	{"TAIL_NUMBER2_001", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"TAIL_NUMBER2_001", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"TAIL_NUMBER2_010", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"TAIL_NUMBER2_010", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"TAIL_NUMBER2_100", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"TAIL_NUMBER2_100", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"TAIL_NUMBER3_001", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"TAIL_NUMBER3_001", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"TAIL_NUMBER3_010", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"TAIL_NUMBER3_010", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"TAIL_NUMBER3_100", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"TAIL_NUMBER3_100", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"NOSE_NUMBER_001", 0 ,"../TaktLwG 66 -  default/M2KC_1T_ada_ch25",false};
+	{"NOSE_NUMBER_001", DECAL ,"../TaktLwG 66 -  default/M2KC_N2_ada_ch25",false};
+
+	{"NOSE_NUMBER_010", 0 ,"../TaktLwG 66 -  default/M2KC_1T_ada_ch25",false};
+	{"NOSE_NUMBER_010", DECAL ,"../TaktLwG 66 -  default/M2KC_N2_ada_ch25",false};
+
+	{"NOSE_NUMBER_100", 0 ,"../TaktLwG 66 -  default/M2KC_1T_ada_ch25",false};
+	{"NOSE_NUMBER_100", DECAL ,"../TaktLwG 66 -  default/M2KC_N2_ada_ch25",false};
+
+	{"WINGR_NUMBER_001", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"WINGR_NUMBER_001", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"WINGR_NUMBER_010", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"WINGR_NUMBER_010", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"WINGR_NUMBER_100", 0 ,"../TaktLwG 66 -  default/M2KC_2T_ada_ch25_SP",false};
+	{"WINGR_NUMBER_100", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"WINGL_NUMBER_001", 0 ,"../TaktLwG 66 -  default/M2KC_1T_ada_ch25",false};
+	{"WINGL_NUMBER_001", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"WINGL_NUMBER_010", 0 ,"../TaktLwG 66 -  default/M2KC_1T_ada_ch25",false};
+	{"WINGL_NUMBER_010", DECAL ,"../TaktLwG 66 -  default/empty",false};
+
+	{"WINGL_NUMBER_100", 0 ,"../TaktLwG 66 -  default/M2KC_1T_ada_ch25",false};
+	{"WINGL_NUMBER_100", DECAL ,"../TaktLwG 66 -  default/empty",false};
+	
+	{"M2000C-19", 0 ,"M2000C_19C_B",true};
+	{"M2000C-20", 0 ,"M2000C_20C_B",true};
+	{"M2000C-22", 0	,"M2000C_22C_2-5",true};
+}
+name = "TaktLwG 66 -  default SP"
